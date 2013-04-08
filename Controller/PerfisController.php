0db28483bdbe47b5dd130615074a24726e3362d8
Adriano Carneiro de Moura <adrianodemoura@gmail.com>
2013-04-08 09:26:17 -0300

    Correção no cadastro de perfis

--- Controller/PerfisController.php
+++ Controller/PerfisController.php
@@ -15,4 +15,15 @@ class PerfisController extends AppController {
 	 * @link	http://book.cakephp.org/2.0/en/controllers.html#components-helpers-and-uses
 	 */
 	public $uses = array('Perfil');
+
+	/**
+	 * Exibe a tela de edição do perfil
+	 * 
+	 * @param	integer		$id		Id do documento a ser editado
+	 */
+	public function editar($id=0)
+	{
+		parent::editar($id);
+		$this->viewVars['focus'] = 'Perfil.nome';
+	}
 }
